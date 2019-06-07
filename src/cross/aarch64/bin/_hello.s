fab94f4b5f13eb53bed00c814bdb04127ee04ee7
David Wildasin <dragonmaus@posteo.net>
2019-06-07 13:29:33 -0400

    minor refactoring

--- src/cross/aarch64/bin/_hello.s
+++ src/cross/aarch64/bin/_hello.s
@@ -4,15 +4,13 @@
 _start:
 	mov	x8, #64		// syscall write
 	mov	x0, #1		// stdout
-	ldr	x1, message
+	adrp	x1, :pg_hi21:message
+	add	x1, x1, #:lo12:message
 	mov	x2, #14
 	svc	#0
 	mov	x8, #93		// syscall exit
 	svc	#0
 
-message:
-	.dword	_message
-
 	.data
-_message:
+message:
 	.ascii	"Hello, world!\n"
--- src/cross/armv7l/bin/_hello.s
+++ src/cross/armv7l/bin/_hello.s
@@ -4,15 +4,13 @@
 _start:
 	mov	r7, #4		// syscall write
 	mov	r0, #1		// stdout
-	ldr	r1, message
+	movw	r1, #:lower16:message
+	movt	r1, #:upper16:message
 	mov	r2, #14
 	svc	#0
 	mov	r7, #1		// syscall exit
 	svc	#0
 
-message:
-	.word	_message
-
 	.data
-_message:
+message:
 	.ascii	"Hello, world!\n"
