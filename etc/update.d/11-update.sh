a8bc5b7558923decfc31feb40f3769987d931490
David Wildasin <dragonmaus@posteo.net>
2019-06-27 09:22:41 -0400

    detect XBPS updates

--- etc/update.d/11-update.sh
+++ etc/update.d/11-update.sh
@@ -1,1 +1,10 @@
+libxbps_version=$(xbps-uhelper version libxbps)
+xbps_version=$(xbps-uhelper version xbps)
+
 sudo xbps-install -u
+
+if [[ $(xbps-uhelper version libxbps) != $libxbps_version || $(xbps-uhelper version xbps) != $xbps_version ]]
+then
+	# XBPS was updated, restart update script
+	exec "$0" "$@"
+fi
