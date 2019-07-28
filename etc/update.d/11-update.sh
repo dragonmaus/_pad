5660c6f4a8304006ee1c48a1216ec61e85822097
David Wildasin <dragonmaus@posteo.net>
2019-07-28 00:30:30 -0400

    Prune old kernels

--- etc/update.d/11-update.sh
+++ etc/update.d/11-update.sh
@@ -1,4 +1,6 @@
 (
+	linux_version1=$(xbps-uhelper version linux)
+	kernel_version1=$(xbps-uhelper version linux${linux_version1%_*})
 	libxbps_version=
 	xbps_version=
 
@@ -9,4 +11,12 @@
 		libxbps_version=$(xbps-uhelper version libxbps)
 		xbps_version=$(xbps-uhelper version xbps)
 	done
+
+	linux_version2=$(xbps-uhelper version linux)
+	kernel_version2=$(xbps-uhelper version linux${linux_version2%_*})
+
+	if [[ $linux_version1 != $linux_version2 || $kernel_version1 != $kernel_version2 ]]
+	then
+		sudo vkpurge rm all
+	fi
 )
