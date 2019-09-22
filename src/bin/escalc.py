0d74af63374a347bf6ba2a3efc3d3bb91418a36b
David Wildasin <dragonmaus@posteo.net>
2019-09-22 00:41:19 -0400

    Update escalc for more reactors

--- src/bin/escalc.py
+++ src/bin/escalc.py
@@ -4,57 +4,91 @@ import sys
 
 heat = {
     'best': None,
-    'large': {
-        'mass': 24,
-        'name': 'Large Heat Shunt',
-        'value': -3240,
-    },
-    'small': {
+    'start': float(sys.argv[2]),
+    'tier1': {
         'mass': 7,
         'name': 'Small Heat Shunt',
         'value': -810,
     },
-    'start': float(sys.argv[2]),
+    'tier2': {
+        'mass': 24,
+        'name': 'Large Heat Shunt',
+        'value': -3240,
+    },
 }
 
 power = {
     'best': None,
-    'large': {
-        'heat': 1920,
-        'mass': 127,
-        'name': 'White Sun Reactor',
-        'value': 1488,
+    'start': float(sys.argv[1]),
+    'tier1': {
+        'heat': 780,
+        'mass': 47,
+        'name': 'Red Sun Reactor',
+        'value': 498,
     },
-    'small': {
+    'tier2': {
         'heat': 1260,
         'mass': 82,
         'name': 'Yellow Sun Reactor',
         'value': 912,
     },
-    'start': float(sys.argv[1]),
+    'tier3': {
+        'heat': 1920,
+        'mass': 127,
+        'name': 'White Sun Reactor',
+        'value': 1488,
+    },
+    'tier4': {
+        'heat': 2340,
+        'mass': 152,
+        'name': 'Blue Sun Reactor',
+        'value': 1878,
+    },
 }
 
-for small in range(1, 21):
-    for large in range(1, 21):
-        value = (power['small']['value'] * small) + (power['large']['value'] * large) + power['start']
-        if value < 0:
-            continue
-        mass = (power['small']['mass'] * small) + (power['large']['mass'] * large)
-        if power['best'] is None or mass < power['best']['mass'] or (mass == power['best']['mass'] and value < power['best']['value']):
-            power['best'] = { 'large': large, 'mass': mass, 'small': small, 'value': value }
+for tier1 in range(0, 51):
+    for tier2 in range(0, 51):
+        for tier3 in range(0, 51):
+            for tier4 in range(0, 51):
+                value = (power['tier1']['value'] * tier1) \
+                      + (power['tier2']['value'] * tier2) \
+                      + (power['tier3']['value'] * tier3) \
+                      + (power['tier4']['value'] * tier4) \
+                      + power['start']
+                if value < 0:
+                    continue
+                mass = (power['tier1']['mass'] * tier1) \
+                     + (power['tier2']['mass'] * tier2) \
+                     + (power['tier3']['mass'] * tier3) \
+                     + (power['tier4']['mass'] * tier4)
+                if power['best'] is None \
+                        or mass < power['best']['mass'] \
+                        or (mass == power['best']['mass'] and value < power['best']['value']):
+                    power['best'] = {
+                        'mass': mass,
+                        'tier1': tier1,
+                        'tier2': tier2,
+                        'tier3': tier3,
+                        'tier4': tier4,
+                        'value': value,
+                    }
 
-heat['start'] += (power['small']['heat'] * power['best']['small']) + (power['large']['heat'] * power['best']['large'])
+for tier in ['tier1', 'tier2', 'tier3', 'tier4']:
+    heat['start'] += power[tier]['heat'] * power['best'][tier]
 
-for small in range(1, 21):
-    for large in range(1, 21):
-        value = (heat['small']['value'] * small) + (heat['large']['value'] * large) + heat['start']
+for tier1 in range(1, 51):
+    for tier2 in range(1, 51):
+        value = (heat['tier1']['value'] * tier1) + (heat['tier2']['value'] * tier2) + heat['start']
         if value >= 0:
             continue
-        mass = (heat['small']['mass'] * small) + (heat['large']['mass'] * large)
+        mass = (heat['tier1']['mass'] * tier1) + (heat['tier2']['mass'] * tier2)
         if heat['best'] is None or mass < heat['best']['mass'] or (mass == heat['best']['mass'] and value < heat['best']['value']):
-            heat['best'] = { 'large': large, 'mass': mass, 'small': small, 'value': value }
+            heat['best'] = { 'tier2': tier2, 'mass': mass, 'tier1': tier1, 'value': value }
 
-print('\t\t"' + power['small']['name'] + '"', power['best']['small'])
-print('\t\t"' + power['large']['name'] + '"', power['best']['large'])
-print('\t\t"' + heat['small']['name'] + '"', heat['best']['small'])
-print('\t\t"' + heat['large']['name'] + '"', heat['best']['large'])
+for tier in ['tier1', 'tier2', 'tier3', 'tier4']:
+    if power['best'][tier] > 0:
+        print('\t\t"' + power[tier]['name'] + '"', power['best'][tier])
+
+for tier in ['tier1', 'tier2']:
+    if heat['best'][tier] > 0:
+        print('\t\t"' + heat[tier]['name'] + '"', heat['best'][tier])
