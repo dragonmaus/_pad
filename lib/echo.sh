8d8eac45d4324a25a993cd75fa8560d35ea1aaf1
David Wildasin <dragonmaus@posteo.net>
2019-12-08 19:09:10 -0500

    Update lib/echo.sh

--- lib/echo.sh
+++ lib/echo.sh
@@ -1,42 +1,57 @@
-case $KSH_VERSION in
-*MIRBSD\ KSH*|*LEGACY\ KSH*)
-  # mksh does not have builtin printf
-  echo() {
-    typeset n=
-    while test $# -gt 0; do
-      case $1 in
-      -n) n=-n ;;
-      --) shift ;&
-      *)  break ;;
-      esac
-      shift
-    done
-    print -R $n "$*"
-  }
-  ;;
-*)
-  echo() (
-    p='%s\n'
-    while test $# -gt 0; do
-      case $1 in
-      -n) p='%s' ;;
-      --) shift
-          break ;;
-      *)  break ;;
-      esac
-      shift
-    done
-    printf "$p" "$*"
-  )
-  ;;
+case "$KSH_VERSION" in
+(*MIRBSD\ KSH*|*LEGACY\ KSH*)
+	# mksh does not have builtin printf
+	echo() {
+		typeset n=
+		while [[ $# -gt 0 ]]
+		do
+			case "$1" in
+			(-n)
+				n=-n
+				;;
+			(--)
+				shift
+				;&
+			(*)
+				break
+				;;
+			esac
+			shift
+		done
+		print -R $n "$*"
+	}
+	;;
+(*)
+	echo() {
+		p='%s\n'
+		while [[ $# -gt 0 ]]
+		do
+			case "$1" in
+			(-n)
+				p='%s'
+				;;
+			(--)
+				shift
+				break
+				;;
+			(*)
+				break
+				;;
+			esac
+			shift
+		done
+		printf "$p" "$*"
+	}
+	;;
 esac
 
 warn() {
-  echo ${1+"$@"} 1>&2
+	echo "$@" 1>&2
 }
+
 die() {
-  r=$1
-  shift 1
-  warn ${1+"$@"}
-  exit $r
+	r="$1"
+	shift 1
+	warn "$@"
+	exit "$r"
 }
