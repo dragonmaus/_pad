ed4da23f9d04cc6d2cfbd558d33b5e719720ea6a
David Wildasin <dragonmaus@posteo.net>
2020-02-13 08:31:17 -0500

    update: Update update script

--- etc/update.d/11-update.sh
+++ etc/update.d/11-update.sh
@@ -1,16 +1,3 @@
 print ">> Updating packages"
-(
-	getversions() {
-		for package
-		do
-			xbps-uhelper version "$package"
-		done
-	}
-	while :
-	do
-		pre="$( getversions libxbps xbps )"
-		sudo xbps-install -u
-		post="$( getversions libxbps xbps )"
-		[[ "$pre" == "$post" ]] && break
-	done
-)
+sudo xbps-install -u xbps
+sudo xbps-install -u
