2fbe624e32fbb55f3b965a998f0df9a9718775aa
David Wildasin <dragonmaus@posteo.net>
2020-02-17 10:44:55 -0500

    Update wine script

--- wine.sh
+++ wine.sh
@@ -1,20 +1,40 @@
 #!/bin/sh
 
+set -e
+
 dir="$HOME/bin/wine"
 mkdir -p "$dir"
 
 while IFS=, read -r n d e
 do
-	p="$( winepath -u "$d\\$e" )"
+  if [[ -d "$HOME/.winefs/base/$n" && -d "$HOME/.winefs/live/$n" ]]
+  then
+    doas "$HOME/.winefs/mount" "$n"
 
-	if [[ -e "$p" ]]
-	then
-		rm -f "$dir/$n.new"
-		printf "#!/bin/sh\\nexec wine start /d '%s' '%s' \"\$@\"\\n" "$d" "$d\\$e" > "$dir/$n.new"
-		chmod +x-w "$dir/$n.new"
-		cmp -s "$dir/$n" "$dir/$n.new" || mv -f "$dir/$n.new" "$dir/$n"
-		rm -f "$dir/$n.new"
-	else
-		rm -f "$dir/$n"
-	fi
+    p="$( winepath -u "$d\\$e" )"
+    if [[ -e "$p" ]]
+    then
+      rm -f "$dir/$n{new}"
+      cat > "$dir/$n{new}" << END
+#!/bin/sh
+set -e
+doas '\$HOME/.winefs/mount' '$n'
+wine start /d '$d' '$d\\$e'
+doas '\$HOME/.winefs/umount' '$n'
+END
+
+      chmod +x-w "$dir/$n{new}"
+
+      cmp -s "$dir/$n" "$dir/$n{new}" || mv -f "$dir/$n{new}" "$dir/$n"
+      rm -f "$dir/$n{new}"
+
+      doas "$HOME/.winefs/umount" "$n"
+
+      continue
+    fi
+
+    doas "$HOME/.winefs/umount" "$n"
+  fi
+
+  rm -f "$dir/$n"
 done < "${0%.sh}.csv"
