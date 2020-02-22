45a78987b8fa2930b728c935355397885c74dc73
David Wildasin <dragonmaus@posteo.net>
2020-02-21 21:06:43 -0500

    Tidy up wine.sh/winefs stuff

--- wine.sh
+++ wine.sh
@@ -2,39 +2,45 @@
 
 set -e
 
-dir="$HOME/bin/wine"
-mkdir -p "$dir"
+bindir="$HOME/bin/wine"
+mkdir -p "$bindir"
 
-while IFS=, read -r n d e
+winefs="$HOME/.winefs"
+
+while IFS=, read -r name path command
 do
-  if [[ -d "$HOME/.winefs/base/$n" && -d "$HOME/.winefs/live/$n" ]]
+  bin="$bindir/$name"
+
+  if [[ ! -d "$winefs/base/$name" || ! -d "$winefs/live/$name" ]]
   then
-    doas "$HOME/.winefs/mount" "$n"
-
-    p="$( winepath -u "$d\\$e" )"
-    if [[ -e "$p" ]]
-    then
-      rm -f "$dir/$n{new}"
-      cat > "$dir/$n{new}" << END
-#!/bin/sh
-set -e
-doas '\$HOME/.winefs/mount' '$n'
-wine start /d '$d' '$d\\$e'
-doas '\$HOME/.winefs/umount' '$n'
-END
-
-      chmod +x-w "$dir/$n{new}"
-
-      cmp -s "$dir/$n" "$dir/$n{new}" || mv -f "$dir/$n{new}" "$dir/$n"
-      rm -f "$dir/$n{new}"
-
-      doas "$HOME/.winefs/umount" "$n"
-
-      continue
-    fi
-
-    doas "$HOME/.winefs/umount" "$n"
+    rm -f "$bin"
+    continue
   fi
 
-  rm -f "$dir/$n"
+  doas "$winefs/mount" "$name"
+
+  wbin="$path\\$command"
+
+  if [[ ! -e "$( winepath -u "$wbin" )" ]]
+  then
+    doas "$winefs/umount"
+    rm -f "$bin{new}"
+    continue
+  fi
+
+  rm -f "$bin{new}"
+  cat > "$bin{new}" << END
+#!/bin/sh
+set -e
+doas '$winefs/mount' '$name'
+wine start /d '$path' '$wbin'
+doas '$winefs/umount'
+END
+
+  chmod +x-w "$bin{new}"
+
+  cmp -s "$bin" "$bin{new}" || mv -f "$bin{new}" "$bin"
+  rm -f "$bin{new}"
+
+  doas "$winefs/umount"
 done < "${0%.sh}.csv"
