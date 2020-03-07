e08d740a6f47b654a66d547f02e78984df6a5701
David Wildasin <dragonmaus@posteo.net>
2020-03-07 15:08:14 -0500

    Remove python2 stuff

--- etc/update.d/41-python.sh
+++ etc/update.d/41-python.sh
@@ -1,6 +1,7 @@
 (
   for i in 2 3
   do
+    which pip$i > /dev/null 2>&1 || continue
     echo ">> Updating python$i packages"
     pip$i install -Uq --no-python-version-warning --upgrade-strategy=eager --user pip
     pip$i list --format=json --no-python-version-warning --not-required --user | jq -r '.[] | .name' | grep -Fvx pip | xargs -r pip$i install -Uq --no-python-version-warning --upgrade-strategy=eager --user
