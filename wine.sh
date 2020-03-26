4db4e7e24cfc7d7fdbccf95d22125f740258415d
David Wildasin <dragonmaus@posteo.net>
2020-03-26 18:40:40 -0400

    Simplify wine framework (again)

--- wine.sh
+++ wine.sh
@@ -1,52 +1,43 @@
 #!/bin/sh
 
 mkexe() {
-  rm -f "$1{new}"
-  cat > "$1{new}"
-  chmod +x-w "$1{new}"
-  cmp -s "$1" "$1{new}" || mv -f "$1{new}" "$1"
-  rm -f "$1{new}"
+  rm -f "$1.new"
+  cat > "$1.new"
+  chmod +x-w "$1.new"
+  cmp -s "$1" "$1.new" || mv -f "$1.new" "$1"
+  rm -f "$1.new"
 }
 
 set -e
 
 bindir="$HOME/bin/wine"
 mkdir -p "$bindir"
-mkexe "$bindir/winefs" << 'END'
-#!/bin/sh
-exec doas "$HOME/.winefs/$1" "$2"
-END
 
-winefs="$HOME/.winefs"
+prefix="$HOME/.wineprefix"
 
 while IFS=, read -r name path command
 do
   bin="$bindir/$name"
+  dir="$prefix/$name"
 
-  if [[ ! -d "$winefs/base/$name" || ! -d "$winefs/live/$name" ]]
+  if [[ ! -d "$dir" ]]
   then
     rm -f "$bin"
     continue
   fi
 
-  winefs mount "$name"
+  export WINEPREFIX="$dir"
 
   wbin="$path\\$command"
 
-  if [[ ! -e "$( winepath -u "$wbin" )" ]]
+  if [[ ! -e "$(winepath -u "$wbin")" ]]
   then
-    winefs umount
-    rm -f "$bin{new}"
+    rm -f "$bin"
     continue
   fi
 
   mkexe "$bin" << END
 #!/bin/sh
-set -e
-winefs mount '$name'
-wine start /d '$path' '$wbin'
-winefs umount
+exec env WINEPREFIX='$dir' wine start /d '$path' '$wbin' "\$@"
 END
-
-  winefs umount
 done < "${0%.sh}.csv"
