0fac21ff98e13c135ca0ec77dffa2184434d4db6
David Wildasin <dragonmaus@posteo.net>
2020-04-25 22:33:15 -0400

    update: Wrap long lines in pip update script

--- etc/update.d/41-python.sh
+++ etc/update.d/41-python.sh
@@ -3,5 +3,8 @@ do
   which pip$i > /dev/null 2>&1 || continue
   echo ">> Updating python$i packages"
   pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user pip
-  pip$i list --format=json --no-python-version-warning --not-required --user | jq -r '.[] | .name' | grep -Fvx pip | xargs -r pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user
+  pip$i list --format=json --no-python-version-warning --not-required --user \
+  | jq -r '.[] | .name' \
+  | grep -Fvx pip \
+  | xargs -r pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user
 done
