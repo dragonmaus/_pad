70c13a012c88df5c3d1522f7337a3834d91f7d0f
David Wildasin <dragonmaus@posteo.net>
2020-12-24 00:15:52 -0500

    Clean up python stuff

--- etc/update.d/01-ssh.sh
+++ etc/update.d/01-ssh.sh
@@ -1,1 +1,1 @@
-env DISPLAY=${DISPLAY:-:0} SSH_ASKPASS=sshpass ssh-add </dev/null
+env DISPLAY=${DISPLAY:-:0} SSH_ASKPASS=sshpass ssh-add < /dev/null
--- etc/update.d/41-python.sh
+++ etc/update.d/41-python.sh
@@ -1,10 +1,4 @@
-for i in 2 3
-do
-  which pip$i > /dev/null 2>&1 || continue
-  echo ">> Updating python$i packages"
-  pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user pip
-  pip$i list --format=json --no-python-version-warning --not-required --user \
-  | jq -r '.[] | .name' \
-  | grep -Fvx pip \
-  | xargs -r pip$i install -U --no-python-version-warning --upgrade-strategy=eager --user
-done
+echo ">> Updating python packages"
+python -m pip list --format=json --no-python-version-warning --not-required \
+| jq -r '.[] | .name' \
+| xargs -r python -m pip install -U --no-python-version-warning --upgrade-strategy=eager --user
